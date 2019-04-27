865eac1de862eb1874f7eb8d7542e7a3cfe752f8
Rohan <almeida@aarohan.biz>
2019-04-27 16:56:28 +0530

    WIP

--- lib/kite_connect/historical_data.ex
+++ lib/kite_connect/historical_data.ex
@@ -4,10 +4,23 @@ defmodule KiteConnect.HistoricalData do
   """
 
   @doc """
-  from_dt = Timex.to_datetime({{2018, 11, 1}, {9, 30, 0}}, "Asia/Kolkata")
-  to_dt = Timex.to_datetime({{2018, 11, 1}, {9, 35, 0}}, "Asia/Kolkata")
+  instrument_token = 5633
+  interval = "minute"
+  
+  Possible values are:
+  · minute
+  · day
+  · 3minute
+  · 5minute
+  · 10minute
+  · 15minute
+  · 30minute
+  · 60minute
 
-  KiteConnect.HistoricalData.get(5633, "minute", from_dt, to_dt)
+  from_dt = "2019-04-01 10:00"
+  to_dt = "2019-04-01 10:15"
+
+  KiteConnect.HistoricalData.get(instrument_token, interval, from_dt, to_dt)
   """
 
   def get(instrument_token, interval, from, to) do
